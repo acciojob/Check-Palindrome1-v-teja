16bd33c4ff68833e4e5da96d285bd82608386ae7
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-03-18 02:13:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // complete the given function
 
 function palindrome(str){
-
+	var start =0;
+	var end = str.length-1;
+	while(start<end){
+		var charA = str.charAt(start);
+		var charB = str.charAt(end);
+		if(charA==' '){
+			start++;
+		}
+		if(charB==' '){
+			end--;
+		}
+		if(charA!=" " && charB!=" "){
+			if(charA!=charB){
+				return true;
+			}
+			start++;
+			end--;
+		}
+	}
+return true;
 }
 module.exports = palindrome
